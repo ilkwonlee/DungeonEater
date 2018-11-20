ad5fedcb481af0adcd810117aef8db9203e65d3b
Seonhyee <hye9e@naver.com>
2018-11-20 16:38:15 +0900

    addCreditButton

---
+++ Assets/Script/CreditScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class button2 : MonoBehaviour {
+	void start() {
+
+	}
+
+	void Update() {
+
+	}
+
+	public void ChangeScene() {
+		SceneManager.LoadScene("CreditScene");
+	}
+}
