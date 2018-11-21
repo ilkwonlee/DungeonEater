55bc21ff15b3eca664514f39d8c9f05b6bf4f719
KwonYG <andamirocll@gmail.com>
2018-11-22 01:09:48 +0900

    add pause key

---
+++ Assets/Script/PauseGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour
+{
+
+    public GameObject PauseUI;
+    private bool paused = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
